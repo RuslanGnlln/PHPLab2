063bcb08caed65ba2397092ff21774eda59d8281
ruslan <ruslan040506@gmail.com>
2022-10-13 11:33:55 +0200

    task 2

--- code/index.php
+++ code/index.php
@@ -1,10 +1,35 @@
 <?php
-/* Imagine a lot of code here */
-$very_bad_unclear_name = "15 chicken wings";
+function task1()
+{
+	/* Imagine a lot of code here */
+	$very_bad_unclear_name = "15 chicken wings";
 
-// Write your code here:
-$order = & $very_bad_unclear_name;
-$order .= " and spicy sauce";
+	// Write your code here:
+	$order = &$very_bad_unclear_name;
+	$order .= " and spicy sauce";
 
-// Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name.";
+	// Don't change the line below
+	echo "\nYour order is: $very_bad_unclear_name.";
+}
+
+function task2()
+{
+	$intA = 9;
+	echo "$intA";
+
+	// Использовал <br/> т.к. \n не переносил строку
+	$intB = 13;
+	echo "<br/>$intB";
+
+	$floatC = 18.47;
+	echo "<br/>$floatC";
+
+	echo "<br/>" . 3 * 4;
+
+	$last_month = 1187.23;
+	$this_month = 1089.98;
+	echo "<br/>";
+	echo $last_month - $this_month;
+}
+
+task2();
